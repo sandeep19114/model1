d646375628cb0c1322bdef49b46652c56583b2f4
Sandeep Anagandula <sandeepanagandula007@gmail.com>
2021-04-08 15:17:23 +0530

    footer-box added

---
+++ frontend/apps/model1/components/footer-box/footer-box.mjs
@@ -0,0 +1,17 @@
+/*
+* (C) 2020 TekMonks. All rights reserved.
+* License: MIT - see enclosed license.txt file.
+*/
+import { router } from "/framework/js/router.mjs";
+import { monkshu_component } from "/framework/js/monkshu_component.mjs";
+import { apimanager as apiman } from "/framework/js/apimanager.mjs";
+import { APP_CONSTANTS } from "../../js/constants.mjs";
+
+
+function register() {
+ // convert this all into a WebComponent so we can use it
+ monkshu_component.register("footer-box",
+`${APP_CONSTANTS.APP_PATH}/components/footer-box/footer-box.html`, footer_box);
+}
+const trueWebComponentMode = true; // making this false renders the component without using Shadow DOM
+export const footer_box = { trueWebComponentMode, register }
